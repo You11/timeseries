32ea5e1e83c335cc1ad5a9cb52a22fad2aa970b2
aa shulpin <aashulpin@gmail.com>
2018-01-17 10:03:34 +0500

    log in and out added, fixed gradle

--- app/src/main/java/ru/you11/timeseries/MainActivity.kt
+++ app/src/main/java/ru/you11/timeseries/MainActivity.kt
@@ -1,12 +1,81 @@
 package ru.you11.timeseries
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val signIn = 200
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == signIn) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Failed to logged in!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_main_login -> {
+                val providers = Arrays.asList(AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build())
+
+                startActivityForResult(AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(), signIn)
+                invalidateOptionsMenu()
+
+                return true
+            }
+
+            R.id.menu_main_logout -> {
+                AuthUI.getInstance().signOut(this).addOnCompleteListener {
+                    Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show()
+                    invalidateOptionsMenu()
+                }
+
+                return true
+            }
+
+            R.id.menu_main_user_profile -> {
+                Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        menu?.clear()
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user == null) {
+            menuInflater.inflate(R.menu.menu_main, menu)
+        } else {
+            menuInflater.inflate(R.menu.menu_main_logged_in, menu)
+        }
+        return true
     }
 }
