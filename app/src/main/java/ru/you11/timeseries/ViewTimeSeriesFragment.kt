35beec91653e1457ad4e4fe598277f7c6b5859d7
aa shulpin <aashulpin@gmail.com>
2018-01-19 13:00:53 +0500

    chart works

--- app/src/main/java/ru/you11/timeseries/ViewTimeSeriesFragment.kt
+++ app/src/main/java/ru/you11/timeseries/ViewTimeSeriesFragment.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.github.mikephil.charting.data.DataSet
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.time_series_card.*
 import kotlinx.android.synthetic.main.view_time_series_fragment.*
@@ -21,14 +26,24 @@ class ViewTimeSeriesFragment: Fragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        val db = FirebaseFirestore.getInstance()
-//        val data = db.collection("time_series").whereEqualTo("uid", arguments.getString("uid")).get().result.documents[0]
-//        val timeSeries = TimeSeries(data["name"].toString(),
-//                data["creationDate"].toString(),
-//                data["changeDate"].toString(),
-//                data["dataValues"],
-//                data["timeValues"])
+        FirebaseFirestore.getInstance().collection("time_series").document(arguments.getString("uid")).get()
+                .addOnCompleteListener {
+                    view_time_series_name.text = it.result["name"].toString()
+                    view_time_series_creation_date.text = it.result["creationDate"].toString()
+                    val entries = ArrayList<Entry>()
+                    val data = it.result["dataValues"] as HashMap<*, *>
+                    data.forEach {
+                        val values = it.value as List<*>
+                        entries.add(Entry(values[0].toString().toFloat(), values[1].toString().toFloat()))
+                    }
 
-        view_time_series_name.text = arguments.getString("uid")
+                    val dataSet = LineDataSet(entries, it.result["dataDescription"].toString())
+                    view_time_series_chart.data = LineData(dataSet)
+                    view_time_series_chart.invalidate()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(activity.applicationContext, "Failed to load data!", Toast.LENGTH_SHORT).show()
+                }
+
     }
 }
