4a5e0c6e98d1d5a6e641a8809cd7bf4a4c09975b
aa shulpin <aashulpin@gmail.com>
2018-01-20 15:25:51 +0500

    view time series screen updated

--- app/src/main/java/ru/you11/timeseries/ViewTimeSeriesFragment.kt
+++ app/src/main/java/ru/you11/timeseries/ViewTimeSeriesFragment.kt
@@ -1,11 +1,14 @@
 package ru.you11.timeseries
 
 import android.app.Fragment
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.DataSet
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
@@ -30,6 +33,7 @@ class ViewTimeSeriesFragment: Fragment() {
                 .addOnCompleteListener {
                     view_time_series_name.text = it.result["name"].toString()
                     view_time_series_creation_date.text = it.result["creationDate"].toString()
+                    //points array
                     val entries = ArrayList<Entry>()
                     val data = it.result["dataValues"] as HashMap<*, *>
                     data.forEach {
@@ -38,12 +42,37 @@ class ViewTimeSeriesFragment: Fragment() {
                     }
 
                     val dataSet = LineDataSet(entries, it.result["dataDescription"].toString())
+                    //chart style
+                    dataSet.lineWidth = 1.5f
+                    dataSet.color = Color.BLUE
+                    dataSet.valueTextSize = 14f
                     view_time_series_chart.data = LineData(dataSet)
+                    view_time_series_chart.axisRight.isEnabled = false
+                    view_time_series_chart.axisLeft.textSize = 14f
+                    view_time_series_chart.axisLeft.axisLineWidth = 1.25f
+                    view_time_series_chart.axisLeft.gridColor = Color.parseColor("#7986CB")
+                    view_time_series_chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+                    view_time_series_chart.xAxis.textSize = 14f
+                    view_time_series_chart.xAxis.axisLineWidth = 1.25f
+                    view_time_series_chart.xAxis.gridColor = Color.parseColor("#7986CB")
+                    //because label is clipped
+                    view_time_series_chart.extraBottomOffset = 5f
+                    view_time_series_chart.description.isEnabled = false
+                    view_time_series_chart.legend.isEnabled = false
                     view_time_series_chart.invalidate()
+                    view_time_series_chart.visibility = View.VISIBLE
+
+                    //add descriptions to axis
+                    view_time_series_x_axis_description.append(it.result["dataDescription"].toString())
+                    view_time_series_x_axis_description.visibility = View.VISIBLE
+                    view_time_series_y_axis_description.append(it.result["timeDescription"].toString())
+                    view_time_series_y_axis_description.visibility = View.VISIBLE
+                    view_screen_loading_icon.hide()
+
                 }
                 .addOnFailureListener {
                     Toast.makeText(activity.applicationContext, "Failed to load data!", Toast.LENGTH_SHORT).show()
+                    view_screen_loading_icon.hide()
                 }
-
     }
 }
